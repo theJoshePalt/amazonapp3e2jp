a9d8bea3ab2d4281cd2fedef53de13fe3d15290f
paltansjosess <paltansjosess@gmail.com>
2025-11-26 15:44:28 +0000

    💄	ui	(_layout): Quitar los 3 botones de navegacion android -Se instalo la libreria de navegacion de android -se edita el _layout de la app para que no aparezcan los 3 iconos de navegacion en la app y que no estorbe en la app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,18 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../lib/providers/AuthProvider";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    // Oculta completamente los botones
+    NavigationBar.setVisibilityAsync("hidden");
+
+    // Evita que vuelvan a aparecer
+    NavigationBar.setBehaviorAsync("overlay-swipe");
+  }, []);
+
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
