1aa98e8c42ee271b532c34e81100974eca7ba386
paltansjosess <paltansjosess@gmail.com>
2025-11-26 16:38:29 +0000

    🎨	style	Cambios de estilo (no funcionales): Dashboard aparecera unas instrucciones de como usar la app en el Dashboard, junto con el nombre de usuario.

--- app/(home)/Dashboard.tsx
+++ app/(home)/Dashboard.tsx
@@ -9,14 +9,85 @@ export default function DashboardScreen() {
     <View className="flex-1 justify-between bg-[#854221] p-6">
       
       {/* Contenido principal */}
-      <View style={{ alignItems: "center", marginTop: 60 }}>
-        <Text className="text-3xl font-bold text-[#280000] mb-3">
+      <View style={{ width: "100%", paddingHorizontal: 20, marginTop: 60 }}>
+
+        {/* TITULO */}
+        <Text
+          style={{
+            fontSize: 34,
+            fontWeight: "bold",
+            color: "#ffffff",
+            textAlign: "center",
+            marginBottom: 12,
+          }}
+        >
           Hola, {user?.username}
         </Text>
 
-        <Text className="text-lg text-[#280000] opacity-80">
-          Bienvenido a tu Dashboard
+        {/* SUBTÍTULO */}
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "600",
+            color: "#ec9b75",
+            textAlign: "center",
+            marginBottom: 25,
+          }}
+        >
+          ¿Qué tenemos pendiente?
         </Text>
+
+        {/* LISTA DE INDICACIONES */}
+        <View style={{ gap: 10 }}>
+    
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#ec9b75",
+            backgroundColor: "#280000",
+            padding: 12,
+            borderRadius: 12,
+          }}
+        >
+          • Pulsa <Text style={{ color: "#ffffff", fontWeight: "bold" }}>TAREAS</Text> para ver lo que has guardado.
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#ec9b75",
+            backgroundColor: "#280000",
+            padding: 12,
+            borderRadius: 12,
+          }}
+        >
+          • Pulsa <Text style={{ color: "#ffffff", fontWeight: "bold" }}>CREAR</Text> para añadir algún deber.
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#ec9b75",
+            backgroundColor: "#280000",
+            padding: 12,
+            borderRadius: 12,
+          }}
+        >
+          • Dale a <Text style={{ color: "#ffffff", fontWeight: "bold" }}>SALIR</Text> para cerrar la app.
+        </Text>
+
+        <Text
+          style={{
+            fontSize: 16,
+            color: "#ec9b75",
+            backgroundColor: "#280000",
+            padding: 12,
+            borderRadius: 12,
+          }}
+        >
+          • Regresa a <Text style={{ color: "#ffffff", fontWeight: "bold" }}>HOME</Text> para ver las instrucciones otra vez.
+        </Text>
+        </View>
       </View>
 
       {/* Tab inferior */}
